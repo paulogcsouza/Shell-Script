219f29dfaf41c3d4260af54f9798ee7e73053fa4
Paulo Giovani Correa de Souza <63620545+paulogcsouza@users.noreply.github.com>
2020-05-11 20:22:34 -0300

    Organizador automatico dos aruivos em Dowload
    script para organizar  de forma autonoma os aquivos no diretório Download

---
+++ Organizador.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+#
+# |Organizaap automatica da pasta de Downloads
+#
+#  Email:      paulogcsouza86@hotmail.com
+#  Autor:      Paulo Souza                 Manutenção: Paulo Souza
+#
+# ---------------------------------------------------------------------------- #
+#  Histórico:
+#   v1.1 11/5/2020 (Começou a funcionar)
+#  Testado em:
+#   bash 4.4.20(1)
+
+# ------------------------------- VARIÁVEIS ------------------------------ #
+VERMELHO="\033[31;1m"
+CIANO="\033[36;1m"
+VAR1=0
+VAR2=0
+VAR3=0
+# ------------------------------- EXECUÇÃO ------------------------------- #
+#######PDF######
+for i in $(find /home/gio/Downloads -maxdepth 1 -name *.pdf);
+do
+
+    let VAR1=VAR1+1
+done
+
+find /home/gio/Downloads -name *.pdf -exec mv {} /home/gio/Documentos \;
+
+echo -e ${VERMELHO} Arquivos .pdf Movidos: ${CIANO}$VAR1
+
+#---------------------------------ZIP----------------------------------------#
+
+for i in $(find /home/gio/Downloads -maxdepth 1 -name *.zip);
+do
+
+    let VAR2=VAR2+1
+done
+
+find /home/gio/Downloads -name *.zip -exec mv {} /home/gio/Documentos \;
+
+echo -e ${VERMELHO} Arquivos .zip Movidos: ${CIANO}$VAR2
+
+#---------------------------------MP4----------------------------------------#
+
+for i in $(find /home/gio/Downloads -maxdepth 1 -name *.mp4);
+do
+
+    let VAR3=VAR3+1
+done
+
+find /home/gio/Downloads -name *.mp4 -exec mv {} /home/gio/Vídeos \;
+
+echo -e ${VERMELHO} Arquivos .mp4 Movidos: ${CIANO}$VAR3
