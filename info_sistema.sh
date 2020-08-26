894936d77c01a96fc738578dab4a8f2eb94ea5fe
Paulo Giovani Correa de Souza <63620545+paulogcsouza@users.noreply.github.com>
2020-08-25 21:16:25 -0300

    Update info_sistema.sh

--- info_sistema.sh
+++ info_sistema.sh
@@ -8,6 +8,7 @@
 # --------------------------------------------------------------------------#
 #  Histórico:
 #   v1.0 21/8/2020
+#   v1.3 25/8/2020
 #   Testado em:
 #   bash 4.4.20(1)
 # -------------------------------VARIÀVEIS--------------------------------- #
@@ -21,6 +22,7 @@ RESPOSTA0=0
 RESPOSTA1=0
 RESPOSTA2=0
 RESPOSTA3=0
+RESPOSTA4=0
 # ---------------------------------TESTE 1---------------------------------- #
 
 echo "Você gostaria de informações sobre o sistema? [s/n] "
@@ -35,27 +37,38 @@ df -h                                    #Uso da memória
 echo
 echo -e "$CINZA Usuários conectados:"
 w                                        #Usuários logados
-echo
+echo ""
 
 # ---------------------------------TESTE 2---------------------------------- #
 
-echo -e "Gostaria de Informações complementares Sobre o Hardware [s/n]"
+echo -e "$AMARELO Gostaria de concultar o calendário ? [s/n]"
 read RESPOSTA1
 test "$RESPOSTA1" != "s" && echo -e "$AMARELO Tenha um bom dia !!!" && exit
-lscpu | sed -n '1,24p' # lista apenas as 24 primeiras linhs (Que são as relevantes para essa função )
-
+echo -e "$CIANO"
+cal
+echo ""
 # ---------------------------------TESTE 3---------------------------------- #
 
-echo -e "Gostaria de Informações Sistama Operacional [s/n]"
+echo -e "$AMARELO Gostaria de Informações complementares Sobre o Hardware [s/n]"
 read RESPOSTA2
 test "$RESPOSTA2" != "s" && echo -e "$AMARELO Tenha um bom dia !!!" && exit
-echo -e "$AZUL Sistema Operacional:"
-lsb_release -a | sed -n '2,4p'
-
+echo -e "$AZUL"
+lscpu | sed -n '1,24p' # lista apenas as 24 primeiras linhs (Que são as relevantes para essa função )
+echo ""
 # ---------------------------------TESTE 4---------------------------------- #
 
-echo -e "Gostaria de Informações Avançadas Sobre os Barramentos [s/n]"
+echo -e "$AMARELOGostaria de Informações Sistama Operacional [s/n]"
 read RESPOSTA3
 test "$RESPOSTA3" != "s" && echo -e "$AMARELO Tenha um bom dia !!!" && exit
+echo -e "$VERMELHO Sistema Operacional:"
+lsb_release -a | sed -n '2,4p'
+echo ""
+# ---------------------------------TESTE 5---------------------------------- #
+
+echo -e "$AMARELO Gostaria de Informações Avançadas Sobre os Barramentos [s/n]"
+read RESPOSTA4
+test "$RESPOSTA4" != "s" && echo -e "$AMARELO Tenha um bom dia !!!" && exit
 echo -e "$CIANO Barramentos:"
 lspci
+echo ""
+echo -e "$AMARELO Tenha um bom dia !!!" && exit
